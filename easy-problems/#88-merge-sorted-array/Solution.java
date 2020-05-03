a59d64199965d72d140e6c35bd6677269412c2a3
raymondpoon95 <36212791+raymondpoon95@users.noreply.github.com>
2020-05-03 22:04:43 +0100

    merge sorted array

--- easy-problems/#88-merge-sorted-array/Solution.java
+++ easy-problems/#88-merge-sorted-array/Solution.java
@@ -3,14 +3,27 @@ import java.util.*;
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        int j = 0;
+        // sort solution takes O(log n) time and O(1) space complexity
+        // int j = 0;
         
-        for(int i = nums1.length-1; i >= m; i--){
-            nums1[i] = nums2[j];
-            j++;
+        // for(int i = nums1.length-1; i >= m; i--){
+        //     nums1[i] = nums2[j];
+        //     j++;
+        // }
+        
+        // Arrays.sort(nums1);
+        
+        int i = m - 1, j = n - 1, length = m + n - 1;
+        
+        while(i >= 0 && j >= 0){
+            if(nums2[j] > nums1[i])
+                nums1[length--] = nums2[j--];
+            else 
+                nums1[length--] = nums1[i--];
         }
         
-        Arrays.sort(nums1);
-        
+        while(j >= 0)
+            nums1[length--] = nums2[j--];
     }
 }
+// the second solution takes O(N) time complexity and O(1) space 
